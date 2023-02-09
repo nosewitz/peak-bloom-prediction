4e8f538a7a0b122977d81b6f0a38e51379c60801
nosewitz <acapecch@gmu.edu>
2023-02-08 23:06:43 -0500

    Added code to read in data

--- cherries.jl
+++ cherries.jl
@@ -1,4 +1,11 @@
 using Pkg; Pkg.activate("."); 
-# Pkg.add(["RCall", "DataFrames"])
+# Pkg.add(["RCall", "DataFrames", "CSV"])
 
-using RCall, DataFrames
+using RCall
+using DataFrames
+using CSV
+
+# append each .csv by reading the directory, "data" and appending it with data\\, create 1 dataframe from all results
+CSV.read.("data\\".*readdir("data")[6:end], DataFrame)
+
+
