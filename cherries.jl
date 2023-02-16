3a5ced7fdc54b77c5bf62d63e48c51da424a3dc7
nosewitz <acapecch@gmu.edu>
2023-02-16 10:55:51 -0500

    Test

--- cherries.jl
+++ cherries.jl
@@ -1,5 +1,5 @@
 import Pkg;
-Pkg.activate("peak-bloom-prediction\\");
+#Pkg.activate("peak-bloom-prediction\\");
 Pkg.instantiate()
 
 
@@ -8,18 +8,19 @@ using DataFrames
 using CSV
 using Chain
 using StatsBase
-using StatPlots
+using StatsPlots
 
 const fpath = "peak-bloom-prediction" 
 
 # append each .csv into 1 DataFrame. 
 # Do this by reading the directory, "data", then appending it with "data\\"
-data = CSV.read("$fpath\\data\\".*readdir("$fpath\\data")[6:end], DataFrame)
+data = CSV.read("data\\".*readdir("data")[6:end], DataFrame)
 
 # restrict looking into four cities 
 keyLocs(x::String) =  any( occursin.(x, ["washingtondc", "Japan/Kyoto", "Switzerland/Liestal", "vancouver"]))
 cities = filter(:location => keyLocs, data) 
 
+theme(:lime)
 # Shows all days are somewhat trending down.
 @df cities scatter(:year,
                    :bloom_doy,
@@ -27,4 +28,13 @@ cities = filter(:location => keyLocs, data)
                    smooth = true,
                    legend = :topright)
 
-                   
+
+x = groupby(cities, :location)
+
+p1 = scatter(x[1].year, x[1].bloom_doy, smooth = true, color = :blue, linewidth=3, label = "Kyoto")
+p2 = scatter(x[2].year, x[2].bloom_doy, smooth = true, color = :red, linewidth=3, label = "Liestal")
+p3 = scatter(x[3].year, x[3].bloom_doy, smooth = true, color = :pink, linewidth=3, label = "Vancouver")
+p4 =scatter(x[4].year, x[4].bloom_doy, smooth = true, color = :green, linewidth=3, label = "Washington D.C.")
+
+
+plot(p1,p2,p3,p4, xlims = (1935, 2030), ylims = (70, 130) )
