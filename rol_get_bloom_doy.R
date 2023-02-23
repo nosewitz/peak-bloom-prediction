5794f9e748e87ffdc403f41f45f97ebdf1adda9e
Richard La Casse <richard_lacasse@hotmail.com>
2023-02-23 15:31:02 -0500

    Update

--- rol_get_bloom_doy.R
+++ rol_get_bloom_doy.R
@@ -3,12 +3,13 @@ library(tidyverse)
 
 kyoto.bloom <- read_csv("data/kyoto.csv") %>% 
   select(year, bloom_doy)
-# japan.kyoto.bloom <- read_csv("data/japan.csv") %>% 
-#   filter(location == "Japan/Kyoto") %>% 
+# japan.kyoto.bloom <- read_csv("data/japan.csv") %>%
+#   filter(location == "Japan/Kyoto") %>%
 #   select(year, bloom_doy)
-# join <- inner_join(kyoto, japan, by = "year")
-# join %>% filter(bloom_doy.x != bloom_doy.y)
-
+# join <- inner_join(kyoto.bloom, japan.kyoto.bloom, by = "year")
+# join <- join %>% filter(bloom_doy.x != bloom_doy.y) %>% 
+#   mutate(diff = bloom_doy.x - bloom_doy.y)
+# sqrt(sqrt(sum((join$diff)^2)))
 
 liestal.bloom <- read_csv("data/liestal.csv") %>% 
   select(year, bloom_doy)
